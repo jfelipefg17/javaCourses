1c2344618849c1a55113f1f11a2d1e074b9a752d
jfelipefg17 <jfelipefg17@gmail.com>
2024-04-16 15:05:14 -0500

    finish first module of testing course

--- Udemy/Testing/Junit5/junit5App/src/test/java/org/example/AccountTest.java
+++ Udemy/Testing/Junit5/junit5App/src/test/java/org/example/AccountTest.java
@@ -70,5 +70,7 @@ class AccountTest {
 
   // assert all , if we have a lot of assert of the same type, with this it will tell you where and what is the problem, no matter how many mistakes
   //assertEquals(expected,actual, () -> "message"); if works not show message, if dont work shows message.
+  //@displayName, to put what will do the test
+  //@Disable, to ignore a test 
 
 }
