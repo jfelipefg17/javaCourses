4781907e58fc6033cfabee42ed097cd6659da0cf
jfelipefg17 <jfelipefg17@gmail.com>
2024-05-31 22:01:38 -0500

    almost finish second module

---
+++ Udemy/WebFlux/IntroReactiveProgramming/SecondExample/src/main/java/com/reactiveApp/SecondExample/SecondExampleApplication.java
@@ -0,0 +1,13 @@
+package com.reactiveApp.SecondExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondExampleApplication.class, args);
+	}
+
+}
